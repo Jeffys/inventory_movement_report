4bb3699ebdd1c2dd4ca172565a6c32a539118af0
alfainkn <alfainkn@gmail.com>
2024-08-06 16:17:28 +0700

    Update optimised query

--- inventory_movement_report/models/product_template.py
+++ inventory_movement_report/models/product_template.py
@@ -14,18 +14,18 @@ class ProductTemplate(models.Model):
         dt_today = date.today()
         first_of_the_month = dt_today.replace(day=1)
         date_debut = first_of_the_month - relativedelta(months=12)
-        list_stock_history_obj = self.env['stock.history.view'].search([('product_template_id', '=', self.id),('date', '>=', date_debut)])
-        list_stock_history = list_stock_history_obj.ids
 
-        _logger.debug("#################### show_pivot_matos 3")
+        self.env['stock.history.view'].recreate_view(self.id)
 
-        name_form = "Stocks Histories"
+        # Fetch the stock history records for the current product template
+        list_stock_history_obj = self.env['stock.history.view'].search([('date', '>=', date_debut)])
+
         return {
-            'name': name_form,
+            'name': "Stocks Histories",
             'res_model': 'stock.history.view',
             'type': 'ir.actions.act_window',
             'view_type': 'form',
             'view_mode': 'graph,pivot,tree',
-            'domain': [('id', 'in', list_stock_history),('product_template_id', '=', self.id)],
+            'domain': [('id', 'in', list_stock_history_obj.ids)],
             'target': 'current',
         }
