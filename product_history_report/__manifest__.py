8b4ea4d5861978a14fbef5026d3148fc34692366
dumblek <duharamadhani@gmail.com>
2024-08-07 14:44:49 +0700

    change module name

--- product_history_report/__manifest__.py
+++ product_history_report/__manifest__.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 {
-    'name': "Inventory Movement Report",
+    'name': "Product History Report",
 
     'summary': """
         This custom Odoo module provides an insightful report on inventory movement over the last year. It offers a comprehensive view of stock changes, enabling better inventory management and decision-making.
